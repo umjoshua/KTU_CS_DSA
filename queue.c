360181af4af3b76fe21703e1bccb395898f2ad54
Joshua Mathew <umjoshua@gmail.com>
2021-01-29 07:56:40 +0530

    Queue using array

--- queue.c
+++ queue.c
@@ -8,23 +8,44 @@ email: umjoshua@gmail.com
 #include<stdlib.h>
 #define max 10
 int queue[max],front=-1,rear=-1;
+
+void display_queue();
+
+int isEmpty(){
+    if (front==-1 || front==rear+1)
+        return 1;
+    else return 0;
+}
+int isFull(){
+    if (rear==max-1)
+        return 1;
+    else return 0;
+}
+
 void enqueue(int num){
     if(front==-1)
         front=0;
-    if(rear==max-1){
-        printf("Queue overflow\n");
-        return;}
+    if(isFull()){
+        printf("\nQueue is full\n");
+        display_queue();return;
+    }
     queue[++rear]=num;
+    display_queue();
 }
 void dequeue(){
-    if(front==-1 || front>rear){
-        printf("Queue underflow..\n");
+    if(isEmpty()){
+        printf("\nQueue is empty\n");
         return;
     }
     front=front+1;
+    display_queue();
 }
 void display_queue(){
-    printf("Queue is: \n");
+    if(isEmpty()){
+        printf("\nQueue is empty\n");
+        return;
+    }
+    printf("\nQueue is: \n");
     for(int i=front;i<=rear;i++){
         printf("%d ",queue[i]);
     }
