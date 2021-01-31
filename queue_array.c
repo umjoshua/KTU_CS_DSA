727fcc58ef77f61ef736360cb427f29530ff5a3c
umjoshua <umjoshua@gmail.com>
2021-01-31 13:17:46 +0530

    Queue using array

--- queue_array.c
+++ queue_array.c
@@ -3,45 +3,44 @@ Name: Queue using array
 Author: Joshua Mathew
 email: umjoshua@gmail.com
 */
-
 #include<stdio.h>
 #include<stdlib.h>
-#define max 10
-int queue[max],front=-1,rear=-1;
+
+int front=-1,rear=-1;
 
 void display_queue();
 
-int isEmpty(){
+int isEmpty(int max){
     if (front==-1 || front==rear+1)
         return 1;
     else return 0;
 }
-int isFull(){
+int isFull(int max){
     if (rear==max-1)
         return 1;
     else return 0;
 }
 
-void enqueue(int num){
-    if(isFull()){
+void enqueue(int queue[],int max,int num){
+    if(isFull(max)){
         printf("\nQueue is full\n");
-        display_queue();return;
+        display_queue(queue,max);return;
     }
     if(front==-1)
         front=0;
     queue[++rear]=num;
-    display_queue();
+    display_queue(queue,max);
 }
-void dequeue(){
-    if(isEmpty()){
+void dequeue(int queue[],int max){
+    if(isEmpty(max)){
         printf("\nQueue is empty\n");
         return;
     }
     front=front+1;
-    display_queue();
+    display_queue(queue,max);
 }
-void display_queue(){
-    if(isEmpty()){
+void display_queue(int queue[],int max){
+    if(isEmpty(max)){
         printf("\nQueue is empty\n");
         return;
     }
@@ -53,22 +52,25 @@ void display_queue(){
 }
 int main(){
     int choice,n;
+    printf("Enter size of queue: ");
+    int max; scanf("%d",&max);
+    int queue[max];
     do{
-    printf("\nEnter your choice:\n");
-    printf("1. Enqueue 2. Dequeue 3. Display 4. Exit : ");
-    scanf("%d",&choice);
-    switch (choice){
-        case 1: printf("Enter the element: ");
-            scanf("%d",&n);
-            enqueue(n);
-            break;
-        case 2: dequeue();
-            break;
-        case 3: display_queue();
-            break;
-        case 4: printf("Exiting..\n");
-            exit(0);
-        default: printf("Invalid choice\n");
+        printf("\nEnter your choice:\n");
+        printf("1. Enqueue 2. Dequeue 3. Display 4. Exit : ");
+        scanf("%d",&choice);
+        switch (choice){
+            case 1: printf("Enter the element: ");
+                scanf("%d",&n);
+                enqueue(queue,max,n);
+                break;
+            case 2: dequeue(queue,max);
+                break;
+            case 3: display_queue(queue,max);
+                break;
+            case 4: printf("Exiting..\n");
+                exit(0);
+            default: printf("Invalid choice\n");
     }
     }while(choice!=4);
 }
